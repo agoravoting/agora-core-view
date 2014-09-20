eeb90a3a031cc7d7a0dc0b5a26c288c7e88c15ce
Javier Llorente <javier@opensuse.org>
2014-09-20 02:32:33 +0200

    make persiana taller (windowHeight

--- playground/persiana/offcanvas.js
+++ playground/persiana/offcanvas.js
@@ -3,3 +3,13 @@ $(document).ready(function () {
     $('.row-offcanvas').toggleClass('active');
   });
 });
+
+$(document).ready(function () {
+  $('.navbar-collapse').on('shown.bs.collapse', function () {
+    windowHeight = $(window).innerHeight();
+    $('.persiana').css('min-height', windowHeight);
+  });
+  $('.navbar-collapse').on('hidden.bs.collapse', function () {
+    $('.persiana').css('min-height', "15px");
+  });
+});
