160d40e4842582ba598cc8ab7479a818e5fac606
Eduardo Robles Elvira <edulix@wadobo.com>
2014-09-22 16:01:36 +0200

    better looking busy box

--- avBooth/busy-directive/busy-directive.js
+++ avBooth/busy-directive/busy-directive.js
@@ -1,8 +1,25 @@
 angular.module('avBooth')
-  .directive('avBusy', function($resource) {
+  .directive('avBusy', function($resource, $window) {
+
+    function link(scope, element, attrs) {
+      scope.onResize = function() {
+        var title = element.find(".avb-busy-title");
+
+        // set margin-top
+        var marginTop = - title.height() - 50;
+        var marginLeft = - title.width()/2;
+        title.attr("style", "margin-top: " + marginTop + "px; margin-left: " + marginLeft + "px");
+      };
+      scope.onResize();
+
+      angular.element($window).bind('resize', function() {
+          scope.onResize();
+      });
+    }
     return {
       restrict: 'E',
       scope: {},
+      link: link,
       transclude: true,
       templateUrl: 'avBooth/busy-directive/busy-directive.html'
     };
