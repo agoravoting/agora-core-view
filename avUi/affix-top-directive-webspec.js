6c7935bbaa451ee1fb70fd43540e8191d589417a
Javier Llorente <javier@opensuse.org>
2014-09-30 10:26:11 +0200

    fix url in e2e tests

--- avUi/affix-top-directive-webspec.js
+++ avUi/affix-top-directive-webspec.js
@@ -2,7 +2,7 @@
 describe("affix-top-directive tests", function () {
 
   beforeEach(function () {
-    browser.get('/#/election/:id/vote/:hash/:message');
+    browser.get('/#/election/1/vote/ff66424d7d77607bbfe78209e407df6fff31abe214a1fe3b3a7dd82600ec0000/8dee0c135afeae29e208550e7258dab7b64fb008bc606fc326d41946ab8e773f:1:1411130040');
   });
 
   it("navbar-unfixed-top is present", function () {
