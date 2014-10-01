606fa27ff6b01aba23f32da518874d6c089d851f
Javier Llorente <javier@opensuse.org>
2014-10-01 12:10:13 +0200

    test multiple resolutions on affix-bottom-directive

--- avUi/affix-bottom-directive-webspec.js
+++ avUi/affix-bottom-directive-webspec.js
@@ -1,16 +1,72 @@
 /* jshint ignore:start */
 describe("affix-bottom-directive tests", function () {
 
-  beforeEach(function () {
-    var html = '<div style="background-color:yellow; height: 500px;">' +
-            '<div av-affix-bottom data-force-affix-width="768"></div>' + '</div>';
-    browser.get('/#/unit-test-e2e?html=' + encodeURIComponent(html));
+  var html;
+  var dataForceAffixWidth;
+
+  function setDataForceAffixWidth(width) {
+    dataForceAffixWidth = width;
+    return '<div style="background-color:yellow; height: 500px;">' +
+            '<div av-affix-bottom data-force-affix-width="' + dataForceAffixWidth + '">' +
+            '</div></div>';
+  }
+
+  function testResolutions() {
+    for (var i = 0; i < 5; i++) {
+      var browserWidth;
+      var browserHeight;
+      switch (i) {
+        case 0:
+          browserWidth = 320
+          browserHeight = 480;
+          break;
+        case 1:
+          browserWidth = 600;
+          browserHeight = 800;
+          break;
+        case 2:
+          browserWidth = 768;
+          browserHeight = 1024;
+          break;
+        case 3:
+          browserWidth = 720;
+          browserHeight = 1280;
+          break;
+        case 4:
+          browserWidth = 1080;
+          browserHeight = 1920;
+          break;
+        default:
+          return;
+      }
+      browser.manage().window().setSize(browserWidth, browserHeight);
+      browser.get('/#/unit-test-e2e?html=' + encodeURIComponent(html));
+    }
+  }
+
+  it("affix-bottom is present (568)", function () {
+    html = setDataForceAffixWidth(568);
+    testResolutions();
   });
 
-  it("affix-bottom is present", function () {
-    browser.manage().window().setSize(320, 480);
-    expect(element(by.css('.affix-bottom')).isPresent()).toBe(true);
+  it("affix-bottom is present (768)", function () {
+    html = setDataForceAffixWidth(768);
+    testResolutions();
   });
+
+  it("affix-bottom is present (868)", function () {
+    html = setDataForceAffixWidth(868);
+    testResolutions();
+  });
+
+  afterEach(function () {
+    if (dataForceAffixWidth > browser.manage().window().width) {
+      expect(element(by.css('.affix-bottom')).isPresent()).toBe(true);
+    } else {
+      expect(element(by.css('.affix-bottom')).isPresent()).toBe(false);
+    }
+  });
+
 
 });
 
