32ffb4ab25c258fc6da52c8bc0bb71cfdd26643b
Eduardo Robles Elvira <edulix@wadobo.com>
2014-10-04 12:51:27 +0200

    booth available options filter now is locale-aware

--- avBooth/available-options-directive/available-options-directive.js
+++ avBooth/available-options-directive/available-options-directive.js
@@ -4,7 +4,7 @@
  * Lists the available options for a question, allowing to change selection.
  */
 angular.module('avBooth')
-  .directive('avbAvailableOptions', function() {
+  .directive('avbAvailableOptions', function($filter) {
 
     var link = function(scope, element, attrs) {
         // initialize selection, only if needed
@@ -34,6 +34,36 @@ angular.module('avBooth')
             option.selected = numSelected;
           }
         };
+
+        // TODO: only use this when localeCompare is unavailable
+        function removeAccents(value) {
+          return value
+            .replace(/á/g, 'a')
+            .replace(/é/g, 'e')
+            .replace(/í/g, 'i')
+            .replace(/ó/g, 'o')
+            .replace(/ú/g, 'u')
+            .replace(/ñ/g, 'n');
+        }
+
+        // filter function that filters option.value ignoring accents
+        function ignoreAccents(item) {
+            if (!scope.filter) {
+              return true;
+            }
+
+            var text = removeAccents(item.value.toLowerCase());
+            var filter = removeAccents(scope.filter.toLowerCase());
+            return text.indexOf(filter) > -1;
+        }
+
+
+        function updateFilteredOptions() {
+          scope.filteredOptions = $filter('filter')(scope.options, ignoreAccents);
+        }
+
+        scope.$watch("filter", updateFilteredOptions);
+        updateFilteredOptions();
     };
 
     return {
