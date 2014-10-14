c90cca8a326c4a4bdb921b9f51c9eab1155691d0
Eduardo Robles Elvira <edulix@wadobo.com>
2014-10-14 19:08:53 +0200

    showing TOS in start screen if available

--- avBooth/start-screen-directive/start-screen-directive.js
+++ avBooth/start-screen-directive/start-screen-directive.js
@@ -4,10 +4,17 @@
  * Shows the steps to the user.
  */
 angular.module('avBooth')
-  .directive('avbStartScreen', function() {
+  .directive('avbStartScreen',  function(ConfigService) {
+
+    function link(scope, element, attrs) {
+        scope.tosTitle = ConfigService.tos.title;
+        scope.tosText = ConfigService.tos.text;
+    }
+
     return {
       restrict: 'AE',
       scope: true,
+      link: link,
       templateUrl: 'avBooth/start-screen-directive/start-screen-directive.html'
     };
   });
