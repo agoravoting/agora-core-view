550bc9d9bb2e31f177e061f94ec244d29f612709
Eduardo Robles Elvira <edulix@wadobo.com>
2014-10-20 10:18:52 +0200

    fixing draft selection dropdown

--- avBooth/selected-options-filter.js
+++ avBooth/selected-options-filter.js
@@ -8,8 +8,17 @@ angular.module('avBooth')
   .filter('avbSelectedOptions', function() {
     return function(optionList) {
       var filtered = _.filter(optionList, function (option) {
-          return option.selected > -1;
+          return option.selected > -1 || option.isSelected === true;
       });
+
+      if (filtered.length === 0) {
+        return [];
+      }
+
+      // if selection is boolean, do not sort by orderer
+      if (filtered[0].isSelected === true) {
+        return filtered;
+      }
 
       return _.sortBy(filtered, function (option) {
         return option.selected;
