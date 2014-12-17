a9403db060ca029c783175b14b724bd5a767ef63
Daniel Garcia Moreno <danigm@wadobo.com>
2014-12-17 13:07:00 +0100

    Removed non existing js inclusions

--- app.js
+++ app.js
@@ -109,12 +109,6 @@ angular.module('agora-core-view').config(
         template: '<div av-plurality-at-large-results></div>',
       });
     $stateProvider
-      .state('test-hmac', {
-        url: '/test_hmac/:key/:hmac/:message',
-        templateUrl: 'test/test_hmac.html',
-        controller: "TestHmacController"
-      });
-    $stateProvider
       .state('unit-test-e2e', {
         url: '/unit-test-e2e',
         templateUrl: 'test/unit_test_e2e.html',
