05bc71235d8582b4804d2beeb98c411c0021f984
Daniel Garcia Moreno <danigm@wadobo.com>
2015-01-07 10:39:05 +0100

    Added angular resolution to 1.3.8

--- avConfig.js
+++ avConfig.js
@@ -7,7 +7,7 @@ var avConfigData = {
   // the base url path for ajax requests, for example for sending ballots or
   // getting info about an election. This url is usually in the form of
   // 'https://foo/api/v3/' and always ends in '/'.
-  baseUrl: "http://agora.dev/temp_data/",
+  baseUrl: "http://agora.dev/elections/api/",
 
   // AuthApi base url
   authAPI: "http://agora.dev/authapi/",
