9dbff20ad5fa684711e6ffd895df0a9780e2bed4
Eduardo Robles Elvira <edulix@wadobo.com>
2015-01-09 11:08:42 +0100

    changing config defaults

--- avConfig.js
+++ avConfig.js
@@ -15,23 +15,23 @@ var avConfigData = {
   electionsAPI: "http://agora.dev/elections/api/",
 
   // default language of the application
-  language: "es",
+  language: "en",
 
   // if we are in debug mode or not
   debug: true,
 
   // contact data where users can reach to a human when they need it
   contact: {
-    email: "contacto@podemos.info",
-    twitter: "ahorapodemos"
+    email: "contact@example.com",
+    twitter: "twitter"
   },
 
   help: {
-    info:"<p>Puedes encontrar más información sobre la votación en los siguientes enlaces:</p><ul><li><a href=\"https://vota.podemos.info/doc/faqs.html\" target=\"_blank\">Preguntas Frecuentes</a></li><li><a href=\"https://vota.podemos.info/doc/auths.html\" target=\"_blank\">Lista de Autoridades</a></li><li><a href=\"https://vota.podemos.info/doc/overview.html\" target=\"_blank\">Esquema General</a></li><li><a href=\"https://vota.podemos.info/doc/tech_overview_1016.pdf\" target=\"_blank\">Descripción técnica (en inglés)</a></li></ul><p>Si quieres, puedes ponerte en contacto con nosotros enviando un email a <a href=\"mailto:contacto@ahorapodemos.info\">contacto@podemos.info</a>.</p>"
+    info:"<p></p>"
   },
 
   success: {
-    text: "<p>¡Enhorabuena! Ya has emitido tu voto con <a href=\"https://agoravoting.com\" target=\"_blank\">Agora Voting</a>. El localizador de tu voto es:</p></h3><p> <strong>{{ballotHash}}</strong></p><p>Puedes usar tu localizador desde la aplicación móvil de podemos para verificar que tu voto ha quedado registrado. Mientras que la votación esté en curso, puedes votar de nuevo, sobreescribiendo e invalidando cualquier voto que hayas emitido anteriormente en esta votación. Para más información sobre la votación:</p><ul><li><a href=\"https://vota.podemos.info/doc/faqs.html\" target=\"_blank\">Preguntas Frecuentes</a></li><li><a href=\"https://vota.podemos.info/doc/auths.html\" target=\"_blank\">Lista de Autoridades</a></li><li><a href=\"https://vota.podemos.info/doc/overview.html\" target=\"_blank\">Esquema General</a></li><li><a href=\"https://vota.podemos.info/doc/tech_overview_1016.pdf\" target=\"_blank\">Descripción técnica (en inglés)</a></li></ul><h2 class=\"text-center\"><a target=\"_blank\" href=\"https://twitter.com/share?url=https://podemos.info\">¡Twitea esta votación!</a></h2></div>"
+    text: "<p>¡Congratulations! You cast your vote with <a href=\"https://agoravoting.com\" target=\"_blank\">Agora Voting</a>.Your ballot locator is:</p></h3><p> <strong>{{ballotHash}}</strong></p>"
   },
 
   tos: {
