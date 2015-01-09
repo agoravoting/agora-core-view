2541d87af6f488498f0414058c7b461648b92fb4
Eduardo Robles Elvira <edulix@wadobo.com>
2015-01-09 09:57:25 +0100

    fix avWidgets

--- avWidgets.js
+++ avWidgets.js
@@ -8,36 +8,23 @@
     return el;
   }
 
-  // convert voting booth widgets
-  var votingBooths = document.getElementsByClassName("agoravoting-voting-booth");
-  for (var i = 0; i < votingBooths.length; i++) {
-    var boothLink = votingBooths[i];
-    var href = boothLink.getAttribute("href");
-    var funcName = boothLink.getAttribute("data-authorization-funcname");
-    window.avRequestAuthorizationFuncName = funcName;
-    var iframe = createElement("iframe", {
-      "class": "agoravoting-voting-booth-iframe",
-      "src": href,
-      "style": "border: 0; width: 100%; height: 100%",
-      "seamless": ""
-    });
-    boothLink.parentNode.insertBefore(iframe, boothLink);
-    boothLink.parentNode.removeChild(boothLink);
-  }
-
-  // convert ballot locator widgets
-  var ballotLocatorLinks = document.getElementsByClassName("agoravoting-ballot-locator");
-  for (var i = 0; i < ballotLocatorLinks.length; i++) {
-    var ballotLocatorLink = ballotLocatorLinks[i];
-    var href = ballotLocatorLink.getAttribute("href");
-    var iframe = createElement("iframe", {
-      "class": "agoravoting-ballot-locator-iframe",
-      "src": href,
-      "style": "border: 0; width: 100%; height: 100%",
-      "seamless": ""
-    });
-    ballotLocatorLink.parentNode.insertBefore(iframe, ballotLocatorLink);
-    ballotLocatorLink.parentNode.removeChild(ballotLocatorLink);
+  function processLinks(className, callback) {
+    var links = document.getElementsByClassName(className);
+    for (var i = 0; i < links.length; i++) {
+      var link = links[i];
+      var href = link.getAttribute("href");
+      if (callback !== undefined) {
+        callback(link, i, className);
+      }
+      var iframe = createElement("iframe", {
+        "class": className + "-iframe",
+        "src": href,
+        "style": "border: 0; width: 100%; height: 100%",
+        "seamless": ""
+      });
+      link.parentNode.insertBefore(iframe, link);
+      link.parentNode.removeChild(link);
+    }
   }
 
   // generic interface for html5 messaging api
@@ -58,5 +45,13 @@
     window[window.avRequestAuthorizationFuncName].apply(window, args);
   }
   window.addEventListener('message', requestAuthorization, false);
+
+  // convert links into widgets
+  processLinks("agoravoting-voting-booth", function (link) {
+    var funcName = link.getAttribute("data-authorization-funcname");
+    window.avRequestAuthorizationFuncName = funcName;
+  });
+  processLinks("agoravoting-ballot-locator");
+  processLinks("agoravoting-results");
 })();
 /* jshint ignore:end */
