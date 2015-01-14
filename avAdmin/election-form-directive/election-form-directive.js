e2c03742a47c61a5cd00579e9096183abd20ee25
Daniel Garcia Moreno <danigm@wadobo.com>
2015-01-14 12:49:12 +0100

    avAdmin: createElection with authapi and agora-elections

--- avAdmin/election-form-directive/election-form-directive.js
+++ avAdmin/election-form-directive/election-form-directive.js
@@ -1,8 +1,72 @@
 angular.module('avAdmin')
-  .directive('avElectionForm', ['Authmethod', '$cookies', function(Authmethod, $cookies) {
+  .directive('avElectionForm', ['Authmethod', 'ElectionsApi', 'AuthApi', '$state', function(Authmethod, ElectionsApi, AuthApi, $state) {
     function link(scope, element, attrs) {
+        scope.newElection = false;
+
         if (!scope.election) {
             scope.election = {questions: []};
+        }
+
+        scope.createToken = null;
+        scope.editToken = null;
+
+        if (!scope.election.id) {
+            scope.newElection = true;
+            AuthApi.getPerm("admin", "election", 0)
+                .success(function(data) {
+                    scope.createToken = data['permission-token'];
+                })
+                .error(function(data) {
+                    // TODO show this error
+                    console.log(data);
+                });
+        } else {
+            AuthApi.getPerm("admin", "election", scope.election.id)
+                .success(function(data) {
+                    scope.editToken = data['permission-token'];
+                })
+                .error(function(data) {
+                    // TODO show this error
+                    console.log(data);
+                });
+        }
+
+        function callError(error) {
+            scope.status = 'Error: ' + error.message;
+            document.querySelector(".error").style.display = "block";
+            scope.loading = false;
+        }
+
+        function createAuthEvent(el, success) {
+            var data = {
+                'name': el.title,
+                'auth_method': 'email', // TODO select method
+            };
+
+            Authmethod.createEvent(data)
+                .success(function(data) {
+                    if (data.status === "ok") {
+                        scope.election.id = data.id;
+                        el.id = data.id;
+                        scope.editToken = data.perm;
+                        success();
+                    } else {
+                        scope.status = 'Not found';
+                        document.querySelector(".error").style.display = "block";
+                        scope.loading = false;
+                    }
+                })
+                .error(callError);
+        }
+
+        function createElection(el) {
+            if (scope.newElection) {
+                ElectionsApi.createElection(el, scope.createToken, scope.editToken,
+                    function(data) {
+                        $state.go("admin.elections");
+                    },
+                    callError);
+            }
         }
 
         function saveElection() {
@@ -21,28 +85,11 @@ angular.module('avAdmin')
             }
 
             if (el.errors.length === 0) {
-                var data = {
-                    'name': el.title,
-                    'auth_method': 'email', // TODO select method
-                    'auth-token': $cookies.auth
-                };
-                Authmethod.createEvent(data)
-                    .success(function(data) {
-                        if (data.status === "ok") {
-                            console.log("OK");
-                            // TODO change other pg
-                        } else {
-                            scope.status = 'Not found';
-                            document.querySelector(".error").style.display = "block";
-                        }
-                    })
-                    .error(function(error) {
-                        scope.status = 'Registration error: ' + error.message;
-                        document.querySelector(".error").style.display = "block";
-                    });
-
+                scope.loading = true;
+                createAuthEvent(el,
+                    // then we create the election
+                    function() { createElection(el); });
             }
-            console.log(el);
         }
 
         angular.extend(scope, {
