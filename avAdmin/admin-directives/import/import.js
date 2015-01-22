d601bdab0c38bca27d2fd4c03c92860c20f198b5
Daniel Garcia Moreno <danigm@wadobo.com>
2015-01-22 10:05:34 +0100

    admin: import page done with fake file upload

--- avAdmin/admin-directives/import/import.js
+++ avAdmin/admin-directives/import/import.js
@@ -2,6 +2,30 @@ angular.module('avAdmin')
   .directive('avAdminImport', function() {
     // we use it as something similar to a controller here
     function link(scope, element, attrs) {
+        scope.loading = false;
+        scope.file = null;
+
+        function selectFile() {
+            document.querySelector("#importfile").click();
+        }
+
+        function uploadFile(element) {
+            scope.loading = true;
+
+            var f = element.files[0];
+            scope.$apply(function() {
+                scope.file = f;
+            });
+
+            // fake upload, TODO make it real
+            console.log(f);
+            setTimeout(function () { scope.$apply(function() { scope.loading = false; }); }, 3000);
+        }
+
+        angular.extend(scope, {
+          selectFile: selectFile,
+          uploadFile: uploadFile
+        });
     }
 
     return {
