0efe164dba1c07e3712c086a8ed25f798db93d50
Eduardo Robles Elvira <edulix@wadobo.com>
2015-01-26 02:29:05 +0100

    improve tabindex in login

--- avRegistration/field-directive/field-directive.js
+++ avRegistration/field-directive/field-directive.js
@@ -5,6 +5,7 @@ angular.module('avRegistration')
   .directive('avrField', function($state) {
     function link(scope, element, attrs) {
       console.log("type = " + scope.field.type);
+      scope.index = attrs.index;
     }
 
     return {
