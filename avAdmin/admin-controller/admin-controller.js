e1b53a0d49d8b1c944c367a1d8a5724fc4c4801f
Daniel Garcia Moreno <danigm@wadobo.com>
2015-01-29 11:55:40 +0100

    Added current election to the admin controller

--- avAdmin/admin-controller/admin-controller.js
+++ avAdmin/admin-controller/admin-controller.js
@@ -2,5 +2,11 @@ angular.module('avAdmin').controller('AdminController',
   function($scope, $state, $stateParams, ElectionsApi) {
     var id = $stateParams.id;
     $scope.state = $state.current.name;
+    $scope.current = null;
+
+    if (id) {
+        ElectionsApi.getElection(id)
+            .then(function(el) { $scope.current = el; });
+    }
   }
 );
