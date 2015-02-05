e00b23b5c552b60d616d6e7e811c82a9425f94bc
Daniel Garcia Moreno <danigm@wadobo.com>
2015-02-05 14:51:11 +0100

    Added question form and question options

--- avAdmin/admin-directives/elquestions/elquestions.js
+++ avAdmin/admin-directives/elquestions/elquestions.js
@@ -8,8 +8,94 @@ angular.module('avAdmin')
             $state.go("admin.census");
         }
 
+        function newq() {
+            var el = ElectionsApi.currentElection;
+            if (!el.questions) {
+                el.questions = [];
+            }
+            // New question
+            var q = {
+                "answer_total_votes_percentage": "over-total-valid-votes",
+                "answers": [],
+                "description": "",
+                "layout": "simple",
+                "max": 1,
+                "min": 1,
+                "num_winners": 1,
+                "randomize_answer_order": true,
+                "tally_type": "plurality-at-large",
+                "title": "New Question " + el.questions.length
+            };
+            el.questions.push(q);
+            expand(el.questions.length - 1);
+        }
+
+        function delq(index) {
+            var el = ElectionsApi.currentElection;
+            var qs = el.questions;
+            el.questions = qs.slice(0, index).concat(qs.slice(index+1,qs.length));
+        }
+
+        function expand(index) {
+            var el = ElectionsApi.currentElection;
+            var qs = el.questions;
+            _.map(qs, function(q) { q.active = false; });
+            qs[index].active = true;
+        }
+
+        function collapse(index) {
+            var el = ElectionsApi.currentElection;
+            var qs = el.questions;
+            _.map(qs, function(q) { q.active = false; });
+        }
+
+        function addOption(index) {
+            var el = ElectionsApi.currentElection;
+            var qs = el.questions;
+            if (!qs[index].answers) {
+                qs[index].answers = [];
+            }
+
+            // new answer
+            var a = {
+                category: "",
+                details: "",
+                id: qs[index].answers.length,
+                sort_order: qs[index].answers.length,
+                text: document.querySelector("#newopt").value,
+                urls: []
+            };
+            qs[index].answers.push(a);
+            document.querySelector("#newopt").value = "";
+        }
+
+        function delOption(i1, i2) {
+            var el = ElectionsApi.currentElection;
+            var qs = el.questions;
+            var as = qs[i1].answers;
+            qs[i1].answers = as.slice(0, i2).concat(as.slice(i2+1,as.length));
+        }
+
+        function incOpt(index, option, inc) {
+            var el = ElectionsApi.currentElection;
+            var qs = el.questions;
+            var q = qs[index];
+
+            if (!q[option]) {
+                q[option] = 0;
+            }
+            q[option] += inc;
+        }
+
         angular.extend(scope, {
           saveQuestions: save,
+          newQuestion: newq,
+          delQuestion: delq,
+          expandQuestion: expand,
+          collapseQuestion: collapse,
+          addOption: addOption,
+          delOption: delOption,
+          incOpt: incOpt,
         });
     }
 
