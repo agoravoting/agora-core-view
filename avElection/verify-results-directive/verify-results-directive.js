8dfda7531870a212bd1be8d9568aaf7b9731ce19
Eduardo Robles Elvira <edulix@wadobo.com>
2015-02-13 01:11:43 +0100

    fix verify results view

--- avElection/verify-results-directive/verify-results-directive.js
+++ avElection/verify-results-directive/verify-results-directive.js
@@ -2,30 +2,17 @@
  * Ballot locator screen directive.
  */
 angular.module('avBooth')
-  .directive('avVerifyResults',  function(ConfigService, $http, $i18next) {
+  .directive('avVerifyResults',  function(ConfigService) {
 
     function link(scope, element, attrs) {
       scope.publicURL = ConfigService.publicURL;
       scope.noHeader = (attrs.noHeader !== undefined);
-
-      if (scope.election || !scope.noHeader) {
-        scope.electionId = scope.election.id;
-        return;
-      }
-
-      scope.electionState = 'loading';
-      $http.get(ConfigService.baseUrl + "election/" + scope.electionId)
-        // on success
-        .success(function(value) {
-          scope.election = value.payload.configuration;
-          scope.electionState = value.payload.state;
-        });
     }
 
     return {
       restrict: 'AE',
-      scope: true,
       link: link,
+      scope: true,
       templateUrl: 'avElection/verify-results-directive/verify-results-directive.html'
     };
   });
