cefbc80c167caf98f43fb1d89fb8adc942e90a47
Daniel Garcia Moreno <danigm@wadobo.com>
2015-02-20 11:21:21 +0100

    Fixing plugin system

--- avAdmin/admin-plugins.js
+++ avAdmin/admin-plugins.js
@@ -10,9 +10,25 @@ angular.module('avAdmin')
             //  directive: 'test', (optional, only if this link has a directive)
             //  head: true | false,
             //  link: ui-sref link,
-            //  menu: html | {icon: icon, text: text}
+            //  menu: html() | {icon: icon, text: text}
             // }
             plugins.plugins.push(p);
+        };
+
+        plugins.reload = function(np) {
+            var ps = plugins.plugins;
+            plugins.plugins = [];
+            ps.forEach(function(p) {
+                if (np.name === p.name) {
+                    plugins.plugins.push(np);
+                } else {
+                    plugins.plugins.push(p);
+                }
+            });
+        };
+
+        plugins.clear = function() {
+            plugins.plugins = [];
         };
 
         return plugins;
