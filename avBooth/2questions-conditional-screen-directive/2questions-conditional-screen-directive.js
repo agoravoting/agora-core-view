b9e84854b22117103f348f6a62823341be1dc303
Eduardo Robles Elvira <edulix@wadobo.com>
2015-02-25 12:16:46 +0100

    allowing up to 4 options in the second question of the 2questions-conditional layout

--- avBooth/2questions-conditional-screen-directive/2questions-conditional-screen-directive.js
+++ avBooth/2questions-conditional-screen-directive/2questions-conditional-screen-directive.js
@@ -17,43 +17,38 @@ angular.module('avBooth')
       });
 
       scope.toggleOption = function(q_num, answ_num) {
-        if (q_num === 0 && answ_num === 0) { // "No"
-          if (scope.election.questions[0].answers[0].selected === -1) { // execute: select
-            scope.election.questions[0].answers[0].selected = 0;
-            scope.election.questions[0].answers[1].selected = -1;
-            if (scope.election.questions.length > 1) {
-              scope.election.questions[1].answers[0].selected = -1;
-              scope.election.questions[1].answers[1].selected = -1;
+        if (q_num === 0) {
+          if (scope.election.questions[0].answers[answ_num].selected === -1) { // execute: select
+            _.each(scope.election.questions[0].answers, function(answer, index) {
+              if (index === answ_num) {
+                answer.selected = 0;
+              } else {
+                answer.selected = -1;
+              }
+            });
+
+            if (scope.election.questions.length > 1 && answ_num === 0) {
+              _.each(scope.election.questions[1].answers, function(answer) {
+                answer.selected = -1;
+              });
             }
           } else { // execute: deselect
-            scope.election.questions[0].answers[0].selected = -1;
+            scope.election.questions[0].answers[answ_num].selected = -1;
           }
-        } else if (q_num === 0 && answ_num === 1) { // "Yes"
-          if (scope.election.questions[0].answers[1].selected === -1) { // execute: select
+        } else if (q_num === 1) {
+          if (scope.election.questions[1].answers[answ_num].selected === -1) { // execute: select
             scope.election.questions[0].answers[0].selected = -1;
             scope.election.questions[0].answers[1].selected = 0;
+
+            _.each(scope.election.questions[1].answers, function(answer, index) {
+              if (index === answ_num) {
+                answer.selected = 0;
+              } else {
+                answer.selected = -1;
+              }
+            });
           } else { // execute: deselect
-            scope.election.questions[0].answers[1].selected = -1;
-            scope.election.questions[1].answers[0].selected = -1;
-            scope.election.questions[1].answers[1].selected = -1;
-          }
-        } else if (q_num === 1 && answ_num === 0) { // "No"
-          if (scope.election.questions[1].answers[0].selected === -1) { // execute: select
-            scope.election.questions[0].answers[0].selected = -1;
-            scope.election.questions[0].answers[1].selected = 0;
-            scope.election.questions[1].answers[0].selected = 0;
-            scope.election.questions[1].answers[1].selected = -1;
-          } else { // execute: deselect
-            scope.election.questions[1].answers[0].selected = -1;
-          }
-        } else if (q_num === 1 && answ_num === 1) { // "Yes"
-          if (scope.election.questions[1].answers[1].selected === -1) { // execute: select
-            scope.election.questions[0].answers[0].selected = -1;
-            scope.election.questions[0].answers[1].selected = 0;
-            scope.election.questions[1].answers[0].selected = -1;
-            scope.election.questions[1].answers[1].selected = 0;
-          } else { // execute: deselect
-            scope.election.questions[1].answers[1].selected = -1;
+            scope.election.questions[1].answers[answ_num].selected = -1;
           }
         }
       };
