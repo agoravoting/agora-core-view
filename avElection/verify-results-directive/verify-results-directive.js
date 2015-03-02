9c13b0abef09c9a7d0400cef4410cd44b2072288
Eduardo Robles Elvira <edulix@wadobo.com>
2015-03-02 01:32:25 +0100

    fix verify results directive

--- avElection/verify-results-directive/verify-results-directive.js
+++ avElection/verify-results-directive/verify-results-directive.js
@@ -2,11 +2,12 @@
  * Ballot locator screen directive.
  */
 angular.module('avBooth')
-  .directive('avVerifyResults',  function(ConfigService) {
+  .directive('avVerifyResults',  function(ConfigService, $stateParams) {
 
     function link(scope, element, attrs) {
       scope.publicURL = ConfigService.publicURL;
       scope.noHeader = (attrs.noHeader !== undefined);
+      scope.electionId = $stateParams.id;
     }
 
     return {
