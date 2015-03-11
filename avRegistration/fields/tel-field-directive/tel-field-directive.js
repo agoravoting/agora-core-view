b61cfdcc669c562051e40c84a8933290e788f437
Eduardo Robles Elvira <edulix@wadobo.com>
2015-03-11 01:39:51 +0100

    fixing tlf field

--- avRegistration/fields/tel-field-directive/tel-field-directive.js
+++ avRegistration/fields/tel-field-directive/tel-field-directive.js
@@ -1,8 +1,12 @@
 angular.module('avRegistration')
   .directive('avrTelField', function($state) {
+    function link(scope, element, attrs) {
+      scope.tlfPattern = /^[+]?\d{6,14}$/;
+    }
     return {
       restrict: 'AE',
       scope: true,
+      link: link,
       templateUrl: 'avRegistration/fields/tel-field-directive/tel-field-directive.html'
     };
   });
