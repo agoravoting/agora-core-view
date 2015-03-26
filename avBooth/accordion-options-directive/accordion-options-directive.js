135552a071ff3928550ffe7c88f3cf731c0d1b75
Eduardo Robles Elvira <edulix@wadobo.com>
2015-03-26 01:33:28 +0100

    ahoram: fix sorting

--- avBooth/accordion-options-directive/accordion-options-directive.js
+++ avBooth/accordion-options-directive/accordion-options-directive.js
@@ -26,20 +26,9 @@ angular.module('avBooth')
         var title = pair[0];
         var answers = pair[1];
 
-        if (scope.layout !== 'ahoram-primaries' || title === 'Candidaturas sin agrupar')
+        if (scope.layout === 'ahoram-primaries' && title !== 'Candidaturas sin agrupar')
         {
-          // randomize orden inside the category
-          // we can't just sample the groupedOptions list because we need to
-          // 1. use the same list object
-          // 2. generate a specific ordering for all the options
-          var shuffledNumbers = _.shuffle(_.map(answers, function () { i += 1; return i;}));
-          // map different sort orders
-          var shuffledAnswers = _.map(shuffledNumbers, function (index) { return answers[index].sort_order;});
-          // now, assign
-          _.each(answers, function (opt, index) { opt.sort_order = shuffledAnswers[index];});
-          answers.sort(function (item1, item2) { return item1.sort_order - item2.sort_order; });
-        } else {
-          answers.sort(function (item1, item2) { return item1.sort_order - item2.sort_order; });
+          answers.sort(function (item1, item2) { return item1.id - item2.id; });
         }
 
         return {
