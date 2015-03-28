e961cbfcefe012b4e7349b716379f6fc568519c3
Eduardo Robles Elvira <edulix@wadobo.com>
2015-03-28 11:48:26 +0100

    fixing ahoramadrid issue #3

--- avBooth/accordion-options-directive/accordion-options-directive.js
+++ avBooth/accordion-options-directive/accordion-options-directive.js
@@ -27,9 +27,20 @@ angular.module('avBooth')
         var title = pair[0];
         var answers = pair[1];
 
-        if (scope.layout === 'ahoram-primaries' && title !== 'Candidaturas sin agrupar')
+        if (scope.layout === 'ahoram-primaries')
         {
-          answers.sort(function (item1, item2) { return item1.id - item2.id; });
+          if (title === 'Candidaturas sin agrupar')
+          {
+            // we can't just sample the groupedOptions list because we need to
+            // 1. use the same list object
+            // 2. generate a specific ordering for all the options
+            var shuffledNumbers = _.shuffle(_.map(answers, function () { i += 1; return i;}));
+            // map different sort orders
+            var shuffledAnswers = _.map(shuffledNumbers, function (index) { return answers[index].sort_order;});
+            // now, assign
+            _.each(answers, function (opt, index) { opt.sort_order = shuffledAnswers[index];});
+          }
+          answers.sort(function (item1, item2) { return item1.sort_order - item2.sort_order; });
         }
 
         return {
--- avBooth/ahoram-primaries-screen-directive/ahoram-primaries-screen-directive.js
+++ avBooth/ahoram-primaries-screen-directive/ahoram-primaries-screen-directive.js
@@ -28,23 +28,6 @@ angular.module('avBooth')
           }).length;
       };
 
-      var question = scope.stateData.question;
-
-      if (question.randomize_answer_order) {
-        // we can't just sample the groupedOptions list because we need to
-        // 1. use the same list object
-        // 2. generate a specific ordering for all the options
-        var i = -1;
-        var answers = question.answers;
-        var shuffledNumbers = _.shuffle(_.map(answers, function () { i += 1; return i;}));
-        // map different sort orders
-        var shuffledAnswers = _.map(shuffledNumbers, function (index) { return answers[index].sort_order;});
-        // now, assign
-        _.each(answers, function (opt, index) { opt.sort_order = shuffledAnswers[index];});
-        answers.sort(function (item1, item2) { return item1.sort_order - item2.sort_order; });
-        scope.stateData.question.answers = answers;
-      }
-
       // questionNext calls to scope.next() if user selected enough options.
       // If not, then it flashes the #selectMoreOptsWarning div so that user
       // notices.
