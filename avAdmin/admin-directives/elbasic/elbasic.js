d78a8fd6155938b9c1aae3bceace33524987d652
Eduardo Robles Elvira <edulix@wadobo.com>
2015-04-12 17:11:02 +0200

    hidden elbasic layout and theme

--- avAdmin/admin-directives/elbasic/elbasic.js
+++ avAdmin/admin-directives/elbasic/elbasic.js
@@ -3,7 +3,7 @@ angular.module('avAdmin')
     // we use it as something similar to a controller here
     function link(scope, element, attrs) {
         scope.election = ElectionsApi.currentElection;
-        scope.layouts = ['simple', /*'2questions-conditional',*/ 'pcandidates-election'];
+        scope.layouts = ['simple', /*'2questions-conditional', 'pcandidates-election'*/];
         scope.themes = ['default'/*, 'podemos'*/];
 
         scope.electionEditable = function() {
