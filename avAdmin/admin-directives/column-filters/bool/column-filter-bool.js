3f8f9d52c3b855db66601c854435bca023fe332c
Eduardo Robles Elvira <edulix@wadobo.com>
2015-04-29 13:45:10 +0200

    admin census: adding sorting/filtering to active column

--- avAdmin/admin-directives/column-filters/bool/column-filter-bool.js
+++ avAdmin/admin-directives/column-filters/bool/column-filter-bool.js
@@ -1,16 +1,17 @@
 angular.module('avAdmin')
-  .directive('avColumnFilterInt', function() {
+  .directive('avColumnFilterBool', function() {
     function link(scope, element, attrs) {
       scope.status = {
         isOpen: false
       };
       scope.filter = {
         sort: '',
-        min: '',
-        max: ''
+        value: ''
       };
       scope.filterPrefix = attrs.filterPrefix;
       scope.filterI18n = attrs.filterI18n;
+      scope.filterI18nNo = attrs.filterI18nNo;
+      scope.filterI18nYes = attrs.filterI18nYes;
 
       function setkey(el, key, val) {
         if (val === '') {
@@ -26,8 +27,7 @@ angular.module('avAdmin')
         }
 
         setkey(scope.filterOptionsVar, scope.filterPrefix + "__sort", scope.filter.sort);
-        setkey(scope.filterOptionsVar, scope.filterPrefix + "__gt", scope.filter.min);
-        setkey(scope.filterOptionsVar, scope.filterPrefix + "__lt", scope.filter.max);
+        setkey(scope.filterOptionsVar, scope.filterPrefix + "__equals", scope.filter.value);
       }, true);
     }
 
@@ -37,6 +37,6 @@ angular.module('avAdmin')
       scope: {
         filterOptionsVar: '='
       },
-      templateUrl: 'avAdmin/admin-directives/column-filters/int/column-filter-int.html'
+      templateUrl: 'avAdmin/admin-directives/column-filters/bool/column-filter-bool.html'
     };
   });
