d99fa554f500f67e31e06e0ccb9b517822fa2c05
Daniel Garcia Moreno <danigm@wadobo.com>
2015-10-24 10:01:25 +0200

    Grouping options by category and subcategory

--- avBooth/colored-accordion-screen-directive/colored-accordion-screen-directive.js
+++ avBooth/colored-accordion-screen-directive/colored-accordion-screen-directive.js
@@ -18,7 +18,46 @@ angular.module('avBooth')
         var q = scope.stateData.question;
         var answers = q.answers;
 
-        var categories = _.groupBy(scope.options, "category");
+        var categories = _.groupBy(answers, "category");
+
+        scope.categories = _.map(_.pairs(categories), function(pair) {
+          var i = -1;
+          var title = pair[0];
+          var answers = pair[1];
+
+          return {
+            title: title,
+            options: answers,
+            isOpen: (scope.folding_policy === "unfold-all")
+          };
+        });
+
+        // getting subcat inside the corresponding cat
+        // scope.categories has this structure:
+        //
+        // cat
+        //  - option1
+        //  - option2
+        //  - subcat (has the attribute: subcat = true)
+        //      - option1
+        //      - option2
+        for (var i=0; i<scope.categories.length; i++) {
+            var c = scope.categories[i];
+            var t = c.title;
+            var spt = t.split(' > ');
+            if (spt.length > 1) {
+                var parent_cat = spt[0];
+                c.subcat = true;
+                for (var j=0; j<scope.categories.length; j++) {
+                    var c1 = scope.categories[j];
+                    if (c1.title === parent_cat) {
+                        c1.options.push(c);
+                        break;
+                    }
+                }
+                scope.categories.pop(i);
+            }
+        }
     };
 
     return {
